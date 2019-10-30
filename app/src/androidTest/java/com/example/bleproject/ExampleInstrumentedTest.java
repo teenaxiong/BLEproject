98572fb2c01b32e29e4bcfec95724b22f3384bbd
teenaxiong <teenaxiong@gmail.com>
2019-10-30 00:06:21 -0400

    delete examples

--- app/src/androidTest/java/com/example/bleproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bleproject/ExampleInstrumentedTest.java
@@ -11,7 +11,7 @@ package com.example.bleproject;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
